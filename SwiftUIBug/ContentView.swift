1c6e8451d85b67b081b28fb3c42cff69921c434e
werb <werbhelius@gmail.com>
2019-12-22 15:24:10 +0800

    Initial Commit

---
+++ SwiftUIBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBug
+//
+//  Created by wanbo on 2019/12/22.
+//  Copyright © 2019 wanbo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
